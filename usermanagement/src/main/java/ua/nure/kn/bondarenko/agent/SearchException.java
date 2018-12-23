1913a853457362c68214b053f1710d5c62a8ac79
AnnaBondarenko98 <anna.bondarenko@nure.ua>
2018-12-23 12:25:11 +0200

    added agent

---
+++ usermanagement/src/main/java/ua/nure/kn/bondarenko/agent/SearchException.java
@@ -0,0 +1,19 @@
+package ua.nure.kn.bondarneko.agent;
+
+
+public class SearchException extends Exception {
+	   
+	public SearchException(String e) {
+	       super(e);
+	   }
+	   
+	   public SearchException() {
+	       super();
+	   }
+
+	   public SearchException(Throwable cause) {
+	       super(cause);
+	   }
+	
+
+}
---
+++ usermanagement/src/main/java/ua/nure/kn/bondarenko/web/DeleteServlet.java
@@ -0,0 +1,7 @@
+package ua.nure.kn.bondarenko.web;
+
+import javax.servlet.http.HttpServlet;
+
+public class DeleteServlet extends HttpServlet {
+
+}
